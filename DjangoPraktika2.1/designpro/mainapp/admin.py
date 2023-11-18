6d7f73b648d67c6f705acc0dd023589da9b59d44
Alkaiir <ivv.ven0@gmail.com>
2023-11-18 11:52:54 +0700

    -

---
+++ DjangoPraktika2.1/designpro/mainapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Category, Application, CustomUser
+
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Application)
+admin.site.register(CustomUser)
